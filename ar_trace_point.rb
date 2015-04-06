1d353e5493f7c81f019bed9ebc8202d75731f477
eileencodes <eileencodes@gmail.com>
2015-04-06 08:18:01 -0400

    Add TracePoint example

---
+++ ar_trace_point.rb
@@ -0,0 +1,35 @@
+require 'active_record'
+require 'minitest/autorun'
+
+ActiveRecord::Base.logger = ActiveSupport::Logger.new($stdout)
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
+ActiveRecord::Base.connection.instance_eval do
+  create_table "users", force: true do |t|
+    t.string "name"
+  end
+
+  create_table "avatars", force: true do |t|
+    t.string  "name"
+    t.integer "user_id"
+  end
+end
+
+class User < ActiveRecord::Base
+  has_one :avatar
+  accepts_nested_attributes_for :avatar
+end
+
+class Avatar < ActiveRecord::Base
+  belongs_to :user
+end
+
+class BugTest < Minitest::Test
+  def test_trace_point
+    user = User.new(name: "My Name")
+
+    user.avatar_attributes = { name: "I am a file name" }
+
+    user.save!
+  end
+end
